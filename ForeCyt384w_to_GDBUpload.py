3fe11bc64b37d74967b96ec3da102b08bdf4c77c
Raymond Howanski <51543419+rjhowanski@users.noreply.github.com>
2024-05-22 16:53:28 -0700

    Add files via upload
    This python script takes input file from iQue/ForeCyt and plate map exported from LIMS to create an upload file to LIMS as an assay run

---
+++ ForeCyt384w_to_GDBUpload.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# author: Raymond Howanski
+# version 1 (2022-aug-11)
+# script purpose is to take forecyt export that utilize SPECIFIC templates 
+# and identify clones located on reformatted (96w to 384w) GDB plate set
+# input files are csv version of forecyt export and txt version of antibodyclonetable of GDB plateset
+# output is a txt file that is uploadable to GDB via the plate set
+
+# import libraries
+import os 
+import pandas as pd
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import * 
+from tkinter import messagebox
+
+# message to instruct first file to select
+messagebox.showinfo("File 1 of 2", "Please select the ForeCyt experiment .csv file.")
+
+# gui to select forecyt experiment file, assumes proper template used
+root = tk.Tk()
+root.withdraw()
+inputfile=filedialog.askopenfilename(filetypes=[('CSV File','.csv')])
+inputfile=os.path.normpath(inputfile)
+datafile = pd.read_csv(inputfile,sep=',',skiprows=0,header=0)
+
+# message to instruct second file to select
+messagebox.showinfo("File 2 of 2", "Please select your GDB Plate Set .txt file.")
+
+# gui to select gdb plate set file
+root = tk.Tk()
+root.withdraw()
+inputfile2=filedialog.askopenfilename(filetypes=[('Txt File','.txt')])
+inputfile2=os.path.normpath(inputfile2)
+platefile=pd.read_csv(inputfile2,sep='\t',header=0)
+
+# create key for datafile and platefile
+datafile['Reference'] = datafile['Plate'] + "_" + datafile['Well Address']
+platefile['Reference'] = platefile['Plate ID'] + "_" + platefile['Well Address']
+
+# keep clones in datafile that exist in GDB platefile
+new_df = datafile[datafile['Reference'].isin(platefile['Reference'])]
+
+# drop reference column
+new_df = new_df.drop('Reference', axis=1)
+
+# change plate column to 'Plate ID' for proper GDB formatting
+new_df.rename(columns = {'Plate':'Plate ID'}, inplace = True)
+
+# count rows in plate set file
+ps_count=len(platefile.index)
+
+# count rows in upload file
+gdb_count=len(new_df.index)
+
+# provides warning if plate set count doesn't match gdb upload file count
+if ps_count != gdb_count:
+    messagebox.showwarning("WARNING", "The number of rows in your upload file ("+str(gdb_count)+") doesn't match rows in your plate set file ("+str(ps_count)+").")
+
+# get path of forecyt experiment file
+head,tail=os.path.split(inputfile)
+name = tail.split('.')[0]
+
+# export gdb upload file as "GDB_Upload_" + experiment file name
+# put in same location of experiment file
+new_df.to_csv(head+'\\GDB_Upload_'+name+'.txt',header=True,sep='\t')
+
+# message displaying number of data rows in upload file
+messagebox.showinfo("Upload Result", "Your .txt file for GDB Upload is ready and contains "+str(gdb_count)+" rows of data")
+
